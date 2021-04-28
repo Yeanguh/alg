805e12d78aa8b7c6993cd80f86635c9d6544b519
伊昂 <yangyuhang@innotechx.com>
2021-04-28 19:39:13 +0800

    feat: add linklist models

--- src/apps/linklist/main.go
+++ src/apps/linklist/main.go
@@ -1,13 +1,25 @@
 package main
 
-import "apps/linklist/models"
+import (
+	"apps/linklist/models"
+	"apps/linklist/single"
+	"fmt"
+)
 
 var LinkList *models.ListNode
 
 func init(){
+	LinkList = &models.ListNode{Val:1,Next:&models.ListNode{Val:2,Next:&models.ListNode{Val:3,Next:&models.ListNode{Val:4,Next:&models.ListNode{Val:5,Next:nil}}}}}
+}
 
+func printLinklist(head *models.ListNode){
+	fmt.Println(head)
+	for head != nil {
+		fmt.Println(head.Val)
+		head = head.Next
+	}
 }
 
 func main(){
-
+	printLinklist(single.Reverse(LinkList))
 }
---
+++ src/apps/linklist/single/reverse.go
@@ -0,0 +1,24 @@
+package single
+
+import "apps/linklist/models"
+
+// 链表反转
+func Reverse(head *models.ListNode) *models.ListNode {
+
+	if head == nil {
+		return nil
+	}
+	var newHead *models.ListNode // 声明一个新的头节点
+	for head != nil {
+		tmp := head.Next // 先把下一个节点临时储存起来
+		head.Next = newHead // 将当前节点next指针指向nil（因为新节点newHead当前为nil），该节点处于孤立，没有节点相连
+		newHead = head // 然后让新节点等于当前节点
+		head = tmp // 再把原节点head移动到下一个节点上，因为已经没有指针指向，直接把开始储存tmp赋值给head，相当于移动操作
+	}
+	return newHead
+}
+
+// https://leetcode-cn.com/problems/palindrome-linked-list/
+func IsPalindrome(head *models.ListNode) bool {
+	return false
+}
